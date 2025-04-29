871dbd45f019ca540e04384d7d23abfdb17d62e2
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2025-04-29 13:05:54 +0300

    first commit

---
+++ src/main/java/com/zeynep/librarymanagementsystem/LibrarymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.zeynep.librarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagementsystemApplication.class, args);
+	}
+
+}
